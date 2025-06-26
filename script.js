db7eafa7e291b310f6aaf1ae17c0aa6ccc32edcf
Waifuofficial <fullxsarapf@gmail.com>
2025-06-26 18:18:46 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const toggle = document.querySelector('.menu-toggle');
+  const menu = document.querySelector('.menu');
+  toggle.addEventListener('click', () => {
+    menu.classList.toggle('open');
+  });
+});
